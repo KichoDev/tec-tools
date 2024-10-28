069b78c69e736be2f4abf5e565be861285638043
KichoDev <msantisdev@gmail.com>
2024-10-27 23:26:17 -0300

    Se programa método show de controlador Technology

--- resources/views/technologies/show.blade.php
+++ resources/views/technologies/show.blade.php
@@ -5,14 +5,14 @@
 @section('content')
 <div class="container">
     <h1>Show Technology</h1>
-    <div>
-        <b>ID:</b>{{ $Technology->id }}
+    <div class="mb-3">
+        <b>ID:</b>{{ $technology->id }}
     </div>
-    <div>
-        <b>Name:</b>{{ $Technology->name }}
+    <div class="mb-3">
+        <b>Name:</b>{{ $technology->name }}
     </div>
-    <div>
-        <b>Description:</b>{{ $Technology->description }}
+    <div class="mb-3">
+        <b>Description:</b>{{ $technology->description }}
     </div>
     <a href="{{ route('technologies.index') }}" class="btn btn-secondary"><i class="bi bi-arrow-left"></i> Back</a>
 </div>
