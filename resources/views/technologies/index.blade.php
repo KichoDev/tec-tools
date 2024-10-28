aabae991b44e5ac6b8ffab221a681dd9d0350637
KichoDev <msantisdev@gmail.com>
2024-10-27 21:44:03 -0300

    Se programa vista index de technologies

--- resources/views/technologies/index.blade.php
+++ resources/views/technologies/index.blade.php
@@ -4,6 +4,33 @@
 
 @section('content')
 <div class="container">
-    <h1>Technologies list</h1>
+    <h1 class="mt-4">Technologies list</h1>
+    <a href="{{ route('technologies.create') }}" class="btn btn-primary">Create</a>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">NAME</th>
+                <th scope="col">DESCRIPTION</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($technologies as $technology)
+            <tr>
+                <th scope="row">{{ $technology->id }}</th>
+                <td>{{ $technology->name }}</td>
+                <td>
+                    <a href="{{ route('technologies.show'), $technology->id }}" class="btn btn-primary">View</a>
+                    <a href="{{ route('technologies.edit'), $technology->id }}" class="btn btn-primary">Edit</a>
+                    <form action="{{ route('technologies.destroy'), $technology->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
