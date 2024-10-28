583a724508d36d5acd6fbe5983d3296a9630f3fc
KichoDev <msantisdev@gmail.com>
2024-10-27 22:20:53 -0300

    Se programa método create de controlador Technology

--- resources/views/technologies/create.blade.php
+++ resources/views/technologies/create.blade.php
@@ -14,8 +14,8 @@
             <label for="description" class="form-label">Description</label>
             <input type="text" class="form-control" id="description" name="description" placeholder="description">
         </div>
-        <button type="submit" class="btn btn-primary"><i class="bi bi-save"></i>Submit</button>
-        <a href="{{ route('technologies.index') }}" class="btn btn-secondary"><i class="bi bi-arrow-left"></i>Back</a>
+        <button type="submit" class="btn btn-primary"><i class="bi bi-save"></i> Submit</button>
+        <a href="{{ route('technologies.index') }}" class="btn btn-secondary"><i class="bi bi-arrow-left"></i> Back</a>
     </form>
 </div>
 @endsection
