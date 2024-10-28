6eaa008b4ca6b24f7931314eab30cee552d0b8e2
KichoDev <msantisdev@gmail.com>
2024-10-27 21:12:55 -0300

    Se crea ruta para método index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TechnologyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/technologies', [TechnologyController::class, 'index'])->name('technologies.index');
